dbd406e5adac5f99ce100136804e42c1a3293265
vilhelmfalkenmark <vilhelmfalkenmark@gmail.com>
2015-12-03 16:35:31 +0100

    Highlightning
    Skjuter in allt via loopar nu istället för hårdkodning. Higlightar
    sökresultat

--- dev/js/jsonScript.js
+++ dev/js/jsonScript.js
@@ -30,35 +30,48 @@ xmlhttp.onreadystatechange = function() {
             centerDiv.innerHTML += students[student].address+"<br> ";
             centerDiv.innerHTML += students[student].postalNumber+"<br> ";
             centerDiv.innerHTML += students[student].city+"<br> ";
+            centerDiv.innerHTML += "Favoritfilm "+students[student].favoriteMovie+"<br> ";
             centerDiv.innerHTML += "Tel "+students[student].phoneNumber+"<br> ";
             gtrDiv.appendChild(centerDiv);
             row.appendChild(gtrDiv);
-            studArr.push(students[student].firstName+students[student].lastName+students[student].address+students[student].postalNumber+students[student].city+students[student].phoneNumber);
+            //  studArr.push(students[student].firstName+students[student].lastName+students[student].address+students[student].postalNumber+students[student].city+students[student].phoneNumber);
+            var concatenatedProps = "";
+            for(var props in students[student])
+            {
+                concatenatedProps = concatenatedProps+students[student][props]+" ";
+
+            }
+            studArr.push(concatenatedProps);
         }
 
         for(var i=0;i<studArr.length;i++)
         {
-            studArr[i] =studArr[i].replace(/\s/g, '');
+           // studArr[i] = studArr[i].replace(/\s/g, '');
             studArr[i] = studArr[i].toLowerCase();
         }
-
+        console.log(studArr);
         /*  START SEARCH  */
         var searchBtn = document.getElementsByClassName("btn")[0];
         var searchResContainer = document.getElementsByClassName("search-result-container")[0];
 
         searchBtn.addEventListener("click",findAddress);
         var searchStatus = document.getElementsByTagName("h3")[0];
+
+        var input2 = document.getElementsByClassName("input")[0];
+        input2.addEventListener("input",findAddress);
+
         function findAddress()
         {
             var hits = "träffar";
-
             var hitIndex = [];
-
             var inputSmall = document.getElementsByClassName("input")[0].value;
             searchResContainer.innerHTML = "";
             var foundHits = 0;
-            inputSmall = inputSmall.replace(/\s/g, '');
+            console.log(inputSmall);
+            //inputSmall = inputSmall.replace(/\s/g, '');
             inputSmall = inputSmall.toLowerCase();
+
+
             for (var j = 0; j<studArr.length;j++)
             {
                 if(studArr[j].indexOf(inputSmall) >= 0 && inputSmall != "")
@@ -73,25 +86,34 @@ xmlhttp.onreadystatechange = function() {
             for (var k = 0;k<hitIndex.length;k++)
             {
                 var searchResult = document.createElement("div");
+                searchResult.className = "search-result";
                 searchResult.innerHTML = students[hitIndex[k]].firstName+ " ";
                 searchResult.innerHTML += students[hitIndex[k]].lastName+"<br>";
                 searchResult.innerHTML += students[hitIndex[k]].address+"<br>";
                 searchResult.innerHTML += students[hitIndex[k]].postalNumber+"<br>";
                 searchResult.innerHTML += students[hitIndex[k]].city+"<br>";
+                searchResult.innerHTML += students[hitIndex[k]].favoriteMovie+"<br>";
                 searchResult.innerHTML += students[hitIndex[k]].phoneNumber+"<br>";
                 foundHits++;
                 searchResContainer.appendChild(searchResult);
             }
-
             searchStatus.innerHTML = "Din sökning gav "+foundHits+" "+hits;
+            var searchResults = document.getElementsByClassName("search-result");
+           // console.log(searchResults);
+            var spanStart = "<span class='highlight'>";
+            var spanEnd = "</span>";
+            var regex = new RegExp(inputSmall,"g");
+
+            for(var h = 0; h<searchResults.length;h++)
+            {
+                searchResults[h].innerHTML = searchResults[h].innerHTML.toLowerCase().replace(regex, spanStart+inputSmall+spanEnd);
+            }
         }
     }
 };
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 container.appendChild(row);
-
-
 
 
 
